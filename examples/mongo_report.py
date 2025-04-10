966489ea5bc2e705c8f28fc89425b2315c0135e4
ElishaKay <kramer1346@gmail.com>
2025-04-10 10:21:16 +0000

    query

--- examples/mongo_report.py
+++ examples/mongo_report.py
@@ -105,7 +105,7 @@ async def run_flow():
     vector_store = create_mongo_vector_store()
 
     # Define your research query
-    query = "who is elishakay of the gpt-researcher github repo?"
+    query = "how would you design a personalization module for a chatbot?"
 
     # Initialize your custom logs handler to stream and save log messages
     custom_logs_handler = CustomLogsHandler(LOGS_COLLECTION)
